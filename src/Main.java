c92a13709c42bf56fd0e2bf6c38f21855f9c5169
Nina Mason <nina12mason@gmail.com>
2025-07-15 13:17:26 -0600

    I added a Route class and changed up the organization of the directory by adding all my java files to a src folder.

---
+++ src/Main.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Main program to run gps distance app.
+ * @author Nina Mason
+ * @version 7/13/2025
+ */
+
+public class Main {
+
+    final static double AVERAGE_SPEED = 30.0;
+
+    public static void main(String args[]) {
+
+        Location coordinate1 = new Location("", 0, 0);
+        Location coordinate2 = new Location("", 0, 0);
+        ArrayList <Route> savedRoutes = new ArrayList<>();
+        int count = 0;
+        String name1 = "";
+        String name2 = "";
+        String input = "";
+        String continueAns = "";
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\n\n---------Welcome to the GPS Distance Calculator---------");
+        do {
+            System.out.println("\nPlease enter the name of the location 1: ");
+            name1 = in.nextLine();
+
+            do {
+                System.out.println("\nPlease enter coordinate 1 as (lat, long): ");
+                input = in.nextLine();
+                coordinate1 = parseCoordinate(input, name1);
+            } while (coordinate1.getLatitude() == 0 && coordinate1.getLongitude() == 0);
+
+            System.out.println("\nPlease enter the name of the location 2: ");
+            name2 = in.nextLine();
+
+            do {
+                System.out.println("\nPlease enter coordinate 2 as (lat, long): ");
+                input = in.nextLine();
+                coordinate2 = parseCoordinate(input, name2);
+            } while (coordinate2.getLatitude() == 0 &&  coordinate2.getLongitude() == 0);
+
+
+            savedRoutes.add(new Route(coordinate1, coordinate2, AVERAGE_SPEED, ("Route " + ++count)));
+
+            System.out.println("\nListing saved routes:\n");
+            for(Route currentRoute : savedRoutes) {
+                System.out.println(currentRoute.toString());
+
+            }
+            System.out.println("\nWould you like to calculate another distance? (y/n)");
+            continueAns = in.nextLine();
+            
+        } while (continueAns.equalsIgnoreCase("y"));
+
+        System.out.println("\n\nThanks for using the GPS distance calculator! Goodbye!\n\n");
+        in.close();
+    }
+
+    /**
+     * Parses coordinates in the form (lat, long) into a Location object.
+     * @param coordinate, The coordinate value in the form (lat, long)
+     * @param name, The name of the coordinate location
+     * @return Location, A new location object with a coordinate and a name
+     */
+    static Location parseCoordinate (String coordinate, String name) {
+        double [] latAndLong = {0,0};
+        if (coordinate.contains(",") && coordinate.contains("(") && coordinate.contains(")")) {
+            try {
+                coordinate = coordinate.replace("(", "").replace(")", "").replace(" ", "");
+                String[] pair = coordinate.split(",");
+                latAndLong[0] = Double.parseDouble(pair[0]);
+                latAndLong[1] = Double.parseDouble(pair[1]);
+                return new Location(name, latAndLong[0], latAndLong[1]);
+                
+            } catch (NumberFormatException e) {
+                System.err.println("Error: Number formatting exception was caught! Please check coordinate input and make sure it is in the form: (lat, long)");
+                return new Location("", 0, 0);
+            }
+            
+        } else {
+            System.out.println("Oops! Invalid input. Make sure to input the coordinates as (lat, long).");
+            return new Location("", 0, 0);
+        }
+        
+
+    }
+}
---
+++ src/Route.java
@@ -0,0 +1,107 @@
+/**
+ * Route class to hold information about a gps route.
+ * @author Nina Mason
+ * @version 7/15/2025
+ */
+public class Route {
+
+    private Location start;
+    private Location end;
+    private double distanceKm;
+    private double distanceMiles;
+    private double timeHrs;
+    private String name;
+
+    final static double R = 6371.0;
+
+    public Route(Location start, Location end, double mph, String name) {
+        this.start = start;
+        this.end = end;
+        double[] distances = haversine(start.getLatitude(), start.getLongitude(), end.getLatitude(), end.getLongitude());
+        this.distanceKm = distances[0];
+        this.distanceMiles = distances[1];
+        this.timeHrs = calculateTime(distanceMiles, mph);
+        this.name = name;
+    }
+
+    public Location getStart() {
+        return start;
+    }
+
+    public Location getEnd() {
+        return end;
+    }
+
+    public double getDistanceKm() {
+        return distanceKm;
+    }
+
+    public double getDistanceMiles() {
+        return distanceMiles;
+    }
+
+    public double getTimeHrs() {
+        return timeHrs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        int hours = (int) timeHrs;
+        int minutes = (int) ((timeHrs - hours) * 60);
+        String output = String.format(name + 
+                                    "| Distance: %.2f km (%.2f miles)     Estimated travel time: %d hr %d min (%.2f hours)%n", 
+                                    distanceKm, distanceMiles, hours, minutes, timeHrs);
+        return output;
+    }
+
+    /**
+     * Calculates the haversine distance between two coordinates.
+     * @param lat1, latitude of coordinate 1
+     * @param lon1, longitude of coordinate 1
+     * @param lat2, latitude of coordinate 2
+     * @param lon2, longitude of coordinate 2
+     * @return double[], the distance in kilometers and miles
+     */
+    public static double[] haversine(double lat1, double lon1, double lat2, double lon2) {
+
+        double[] results = new double[2];
+
+        // Convert degrees to radians
+        double lat1Rad = Math.toRadians(lat1);
+        double lon1Rad = Math.toRadians(lon1);
+        double lat2Rad = Math.toRadians(lat2);
+        double lon2Rad = Math.toRadians(lon2);
+
+        // Differences
+        double dLat = lat2Rad - lat1Rad;
+        double dLon = lon2Rad - lon1Rad;
+
+        // Haversine formula
+        double a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.cos(lat1Rad) * Math.cos(lat2Rad)
+                * Math.pow(Math.sin(dLon / 2), 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        results[0] = R * c;
+        results[1] = R * c * 0.621371; // Convert km to miles
+        return results;
+    }
+
+    /**
+     * Calculates estimated travel time in hours.
+     * @param miles, distance in miles
+     * @param mph, average speed in miles per hour
+     * @return double, the time a route takes in hours
+     */
+    public static double calculateTime (double miles, double mph) {
+        if (mph != 0) {
+            return (miles/mph);
+        } else {
+            return 0;
+        }
+    }
+
+}
