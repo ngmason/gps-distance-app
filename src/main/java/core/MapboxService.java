3f9e0bc0ca3bcc7f187e5efd766eb84717f7a8e9
Nina <nina12mason@gmail.com>
2026-07-03 14:18:21 -0600

    Add MapboxService multi-waypoint routing support

--- src/main/java/core/MapboxService.java
+++ src/main/java/core/MapboxService.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * Builds the map using the Mapbox and Mapbox Directions API.
@@ -36,30 +37,33 @@ public class MapboxService {
      * @param latB, latitude from coordinate 2
      * @return String, encoded polyline as a String
      */
-    public String getEncodedPolyline(double lonA, double latA, double lonB, double latB) throws Exception {
-        String urlStr = String.format(
-            BASE_URL + "%f,%f;%f,%f?geometries=polyline&overview=simplified&access_token=%s",
-            lonA, latA, lonB, latB, token
-        );
-
+    public String getEncodedPolyline(List<Location> locations) throws Exception {
+        if (locations.size() < 2) throw new IllegalArgumentException("At least 2 locations required");
+        StringBuilder coords = new StringBuilder();
+        for (int i = 0; i < locations.size(); i++) {
+            if (i > 0) coords.append(";");
+            Location loc = locations.get(i);
+            coords.append(String.format("%f,%f", loc.getLongitude(), loc.getLatitude()));
+        }
+        String urlStr = BASE_URL + coords + "?geometries=polyline&overview=simplified&access_token=" + token;
         URL url = new URL(urlStr);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-
         try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
             StringBuilder response = new StringBuilder();
             String line;
             while ((line = in.readLine()) != null) {
                 response.append(line);
             }
-
             JSONObject json = new JSONObject(response.toString());
             JSONObject route = json.getJSONArray("routes").getJSONObject(0);
-
             String polyline = route.getString("geometry");
-
             return URLEncoder.encode(polyline, StandardCharsets.UTF_8.toString());
         }
+    }
+
+    public String getEncodedPolyline(double lonA, double latA, double lonB, double latB) throws Exception {
+        return getEncodedPolyline(List.of(new Location("", latA, lonA), new Location("", latB, lonB)));
     }
 
     /**
@@ -74,22 +78,27 @@ public class MapboxService {
      * @param zoom, amount of zoom the map should create around the points
      * @return String, the URL that will be used to render the map
      */
+    public String buildStaticMapUrl(String encodedPolyline, List<Location> locations,
+                                    double centerLon, double centerLat, int zoom) {
+        StringBuilder overlays = new StringBuilder();
+        overlays.append("path-5+ff0000-0.8(").append(encodedPolyline).append(")");
+        for (int i = 0; i < locations.size(); i++) {
+            Location loc = locations.get(i);
+            String color = (i == 0) ? "ff0000" : (i == locations.size() - 1) ? "0000ff" : "ff8c00";
+            overlays.append(String.format(",pin-s+%s(%f,%f)", color, loc.getLongitude(), loc.getLatitude()));
+        }
+        return String.format("%s%s/%f,%f,%d/600x400?access_token=%s",
+            STYLE_URL, overlays, centerLon, centerLat, zoom, token);
+    }
+
     public String buildStaticMapUrl(String encodedPolyline,
                                     double lonA, double latA,
                                     double lonB, double latB,
-                                    double centerLon, double centerLat,
-                                    int zoom) {
-        return String.format(
-            "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/"
-        + "path-5+ff0000-0.8(%s),"
-        + "pin-s+ff0000(%f,%f),"
-        + "pin-s+0000ff(%f,%f)/%f,%f,%d/600x400?access_token=%s",
+                                    double centerLon, double centerLat, int zoom) {
+        return buildStaticMapUrl(
             encodedPolyline,
-            lonA, latA,
-            lonB, latB, 
-            centerLon, centerLat,
-            zoom,
-            token 
+            List.of(new Location("", latA, lonA), new Location("", latB, lonB)),
+            centerLon, centerLat, zoom
         );
     }
 
